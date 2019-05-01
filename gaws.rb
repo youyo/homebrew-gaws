15fca88b994b4588d1f42950c6c039c2e6757dca
youyo <1003ni2@gmail.com>
2019-05-01 06:23:17 +0000

    [skip ci] bump up 0.0.5

--- gaws.rb
+++ gaws.rb
@@ -1,8 +1,8 @@
 class Gaws < Formula
   desc "gaws is a command to complement aws-cli."
   homepage "https://github.com/youyo/gaws"
-  url "https://github.com/youyo/gaws/releases/download/0.0.4/gaws_darwin_amd64.zip"
-  sha256 "bdcf244f7ced8324af5472bf5cc397bb740ab8659c7e1b5a0db8c34dbacf6849"
+  url "https://github.com/youyo/gaws/releases/download/0.0.5/gaws_darwin_amd64.zip"
+  sha256 "72544d955bfc39887b1a940e3ba4d251c25f6393bc0025fceb8b8fd24f06f7c2"
 
   def install
     bin.install "gaws"
